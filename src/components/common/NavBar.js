e4f70427d6967420c65ae02cd41bdab4bc042efb
BlisS <contacto@fixter.org>
2017-05-27 13:43:26 -0500

    navbar added

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -3,18 +3,52 @@
  */
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import { Link } from 'react-router-dom';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
 
-const NavBar = (...props) => {
-    return (
-        <div>
-            <AppBar
-                title="Reto Zapopan"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-            />
-        </div>
-    );
-};
+
+class NavBar extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            open: false
+        }
+    }
+    handleToggle = () => this.setState({open: !this.state.open});
+
+    handleClose = () => this.setState({open: false});
+
+    render(){
+        return (
+            <div>
+                <AppBar
+                    title="Reto Zapopan"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onLeftIconButtonTouchTap={this.handleToggle}
+                />
+                <Drawer
+                    docked={false}
+                    width={200}
+                    open={this.state.open}
+                    onRequestChange={(open) => this.setState({open})}
+                >
+                    <Toolbar style={{backgroundColor:'red'}}>
+                        <ToolbarTitle text="Menú" />
+                    </Toolbar>
+                    <Link to="/">
+                        <MenuItem onTouchTap={this.handleClose}>Inicio</MenuItem>
+                    </Link>
+                    <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+                </Drawer>
+            </div>
+        );
+    }
+
+}
 
 NavBar.propTypes = {};
 
