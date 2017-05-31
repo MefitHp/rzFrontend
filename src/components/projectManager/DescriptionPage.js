ba26d459f30cb961ceb492b656598d6818ffad8e
BlisS <contacto@fixter.org>
2017-05-31 11:57:55 -0500

    mobile markdown editor

--- src/components/projectManager/DescriptionPage.js
+++ src/components/projectManager/DescriptionPage.js
@@ -3,14 +3,27 @@ import {Toolbar, ToolbarTitle} from 'material-ui/Toolbar';
 import {cyan500} from 'material-ui/styles/colors';
 import ReactMarkdown from 'react-markdown';
 import { RaisedButton, ToolbarGroup } from 'material-ui';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import ActionSettingsEthernet from 'material-ui/svg-icons/action/settings-ethernet';
+import ActionVisibility from 'material-ui/svg-icons/action/visibility';
+
 
 
 
 class DescriptionPage extends Component{
 
     state = {
-        input: '# cochinon\n\n> cita loquilla\n\n![bliss](http://localhost:3000/static/media/bliss.06322ec7.jpg)'
+        input: '# cochinon\n\n>cita loquilla\n\n![bliss](http://localhost:3000/static/media/bliss.06322ec7.jpg)\n\n<ul>\n<li>chet</li>\n</ul>\n\n* pollo rojo\n\n* pollo verde \n\n* pollo azul\n\n> simplemente quote\n\n**ño morro**\n\nwhat about simple paragraph??',
+        selectedIndex: 0,
+        ancho: document.documentElement.clientWidth < 600,
+        desapareceMark: '',
+        desaparecePrev: ''
+
     };
+
+    select = (index) => this.setState({selectedIndex: index});
+
 
     onChange = (e) => {
        this.setState({
@@ -31,6 +44,8 @@ class DescriptionPage extends Component{
 
         s1.addEventListener('scroll', select_scroll_1, false);
         // s2.addEventListener('scroll', select_scroll_2, false);
+        // desaparecemos el preview en mobiles
+        if(this.state.ancho) this.setState({desaparecePrev: 'desaparece'})
 
 
     }
@@ -56,7 +71,7 @@ class DescriptionPage extends Component{
                 <div className="desc-container">
 
                     <div
-                        className="desc-editor"
+                        className={"desc-editor " + this.state.desapareceMark}
 
                     >
                        <textarea
@@ -70,7 +85,7 @@ class DescriptionPage extends Component{
                      <br/>
                     <div
                         id="Select2"
-                        className="mark desc-preview">
+                        className={"mark desc-preview " + this.state.desaparecePrev}>
                         <ReactMarkdown
                             id="mijo"
                             style={{color:'red'}}
@@ -83,7 +98,43 @@ class DescriptionPage extends Component{
 
 
 
+                {/*Switch de visibilidad si mobile*/}
+                {this.state.ancho && <Paper
+                    style={{
+                        position:'fixed',
+                        bottom:0,
+                        width:'100%'
+                    }}
+                    zDepth={1}>
+                    <BottomNavigation selectedIndex={this.state.selectedIndex}>
+                        <BottomNavigationItem
+                            label="Markdown"
+                            icon={<ActionSettingsEthernet/>}
+                            onTouchTap={() => {
+                                this.select(0);
+                                this.setState({
+                                    desaparecePrev: 'desaparece',
+                                    desapareceMark:''
+                                });
+                            }}
+                        />
+                        <BottomNavigationItem
+                            label="Preview"
+                            icon={<ActionVisibility/>}
+                            onTouchTap={() =>{
+                                this.select(1);
+                                this.setState({
+                                    desapareceMark:'desaparece',
+                                    desaparecePrev: ''
+                                });
+                            }}
+                        />
+                    </BottomNavigation>
+                </Paper>}
+
+
             </div>
+
         );
     }
 }
