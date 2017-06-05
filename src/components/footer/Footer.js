f2defb3f9cff4dd0493da3547ec9370e435b5549
Brenda Gonzalez <bremin11.20.93@gmail.com>
2017-06-05 16:20:47 -0500

    fin maquetado

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -19,25 +19,12 @@ class Footer extends React.Component {
 					empresas e iniciativas valiosas, relevantes y con valor agregado.</p>
 
        			</div>
+       
        			<div className='definition'>
-       				<h4> Reto Zapopan</h4>
-       				<p> Somos un programa impulsado por el Gobierno Municipal de Zapopan,
-       				 que a través de una competencia de ideas, buscamos impulsar, 
-       				 de manera gratuita, a los emprendedores a través de un programa integral 
-       				 de consultoría, mentoría, financiamiento, vinculación e internacionalización.
-       				  Buscamos emprendedores que destaquen por sus ideas,
-					empresas e iniciativas valiosas, relevantes y con valor agregado.</p>
-
-       			</div>
-       			<div className='definition'>
-       				<h4> Reto Zapopan</h4>
-       				<p> Somos un programa impulsado por el Gobierno Municipal de Zapopan,
-       				 que a través de una competencia de ideas, buscamos impulsar, 
-       				 de manera gratuita, a los emprendedores a través de un programa integral 
-       				 de consultoría, mentoría, financiamiento, vinculación e internacionalización.
-       				  Buscamos emprendedores que destaquen por sus ideas,
-					empresas e iniciativas valiosas, relevantes y con valor agregado.</p>
-
+       				<h4> Nos encontramos en</h4>
+              <div className='inf_flex'><FontIcon className='material-icons inf_z' style={{color:'white', fontSize:'20'}}><span>place</span></FontIcon><p>Av. Vallarta No.6503, Plaza Concentro, Local E-48, Zapopan, Jalisco.</p></div>
+              <div className='inf_flex'><FontIcon className='material-icons inf_z' style={{color:'white', fontSize:'20'}}><span>email</span></FontIcon><p>contacto@retozapopan.com</p></div>
+              <div className='inf_flex'><FontIcon className='material-icons inf_z' style={{color:'white', fontSize:'20'}}><span>phone_in_talk</span></FontIcon><p>3818.2200 ext.2822 / 2821 / 2816</p></div>
        			</div>
        		</div>
     		<hr/>
