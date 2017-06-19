9f7afcfebd3980627035bd4f74056ab4d83bd702
Oswaldinho <oswalfut_96@hotmail.com>
2017-06-18 22:30:49 -0500

    modification almost finished

--- src/components/adminPanel/validateProject.js
+++ src/components/adminPanel/validateProject.js
@@ -5,7 +5,11 @@ import Paper from 'material-ui/Paper';
 import Avatar from 'material-ui/Avatar';
 import Chip from 'material-ui/Chip';
 import {ListItem} from 'material-ui/List';
-
+import Divider from 'material-ui/Divider';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
 
 
 class ValidateProject extends Component{
@@ -13,6 +17,7 @@ class ValidateProject extends Component{
   constructor(){
     super();
     this.state={
+      value:1,
       project:{
         name:'',
         category:[{
@@ -41,11 +46,11 @@ class ValidateProject extends Component{
     .catch(
         e=>{
             console.log(e);
-
         }
-
     );
   }
+
+   handleChange = (event, index, value) => this.setState({value});
   render(){
     return(
 
@@ -72,15 +77,52 @@ class ValidateProject extends Component{
                 </GridTile>
                 <GridTile cols={2}>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
                 </GridTile>
               </GridList>
             </Paper>
 
           </GridTile>
           <GridTile cols={1} style={{padding:'0 2% 2% 2%'}}>
-            <Paper zDepth={1} style={{height:'85vh'}}>
-              {this.state.author.username}
+            <Paper zDepth={1}
+              style={{padding:'1%',height:'85vh', display:'flex', flexDirection:'column', justifyContent:'space-around'}}>
+              <div>
+                <ListItem disabled={true} primaryText="Status" leftIcon={<ContentInbox />} />
+                  <SelectField
+                    style={{paddingLeft:'5%'}}
+                   floatingLabelText="El estado actual será"
+                   value={this.state.value}
+                   autoWidth={true}
+                   onChange={this.handleChange}
+                 >
+                   <MenuItem value={1} primaryText="Never" />
+                   <MenuItem value={2} primaryText="Every Night" />
+                   <MenuItem value={3} primaryText="Weeknights" />
+                   <MenuItem value={4} primaryText="Weekends" />
+                   <MenuItem value={5} primaryText="Weekly" />
+                 </SelectField>
+                <Divider style={{width:'100%'}} />
+              </div>
+              <div>
+                <ListItem disabled={true} primaryText="Meta" leftIcon={<ContentInbox />} />
+                  <TextField
+                    style={{paddingLeft:'5%'}}
+                    hintText="$100000"
+                    floatingLabelText="Meta Actual"
+                  /><br />
+                <Divider style={{width:'100%'}} />
+              </div>
+              <div
+                style={{overflow:'scroll'}}>
+                <ListItem disabled={true} primaryText="Observaciones" leftIcon={<ContentInbox />} />
+                  <TextField
+                    style={{paddingLeft:'5%'}}
+                    hintText="Ser mas claro en..."
+                    floatingLabelText="El emprendedor deberá..."
+                    multiLine={true}
+                    rows={5}
+                  /><br />
+              </div>
             </Paper>
           </GridTile>
         </GridList>
