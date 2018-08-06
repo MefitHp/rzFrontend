c41d4b957a7db17e28d9e61e0c3fd96da87121b6
Brenda Gonzalez <bremin11.20.93@gmail.com>
2018-08-06 00:47:45 -0500

    cards

---
+++ src/components/card/CardRelevantesDisplay.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Card.css';
+import cometa from '../../assets/cometa.png';
+import bandera from '../../assets/bandera.png';
+
+export const CardRelevantesDisplay = () => (
+    <div className="destacado ">
+        <div className="rl_img">
+        </div>
+        <div className="rl_data">
+            <h2>Desarrollo de primeros 100 lentes de realidad virtual: lentes</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque inventore optio recusandae. Accusamus adipisci aliquid eveniet ex maxime quia quidem totam unde. Aperiam commodi, ipsa ipsam natus numquam obcaecati repudiandae.
+            </p>
+            <br/>
+            <div>
+
+            </div>
+        </div>
+    </div>
+);
