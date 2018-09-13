9f32e1d2619f63c39e5c59c345bca42dd1b1f3c7
MefitHp <mefitDev@gmail.com>
2018-09-12 19:49:09 -0500

    Campaig creator thumbnail

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './components/home/HomePage';
-import {UserProfilePage} from './components/userProfile/UserProfilePage';
+import UserProfile from './components/userProfile/UserProfile';
 import CreateProject from './components/createProject/CreateProject';
 import ProjectsPage from './components/projectListing/ProjectsPage';
 import HomeDisplay from './components/home/HomeDisplay';
 import NavContainer from './components/nav/NavContainer';
 import PrivateRoute from './PrivateRouteConnect';
 //import ProjectManagerContainer from './components/projectManager/ProjectManagerContainer';
-import {ManagerPage} from './components/projectManager/ProjectManagerContainer';
+import { ManagerPage } from './components/projectManager/ProjectManagerContainer';
 import LoginPage from './components/login/LoginPage';
 //import DetailPage from './components/projectListing/DetailPage';
 import BlissDetailPage from './components/bliss/detail/BlissDetailPage';
@@ -22,33 +22,34 @@ import imgerror from './assets/404.png';
 
 const noMatch = () => (
   <div style={{
-      backgroundImage: 'url('+imgerror+')',
-      backgroundSize: 'contain',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      height: '100vh'}}>
+    backgroundImage: 'url(' + imgerror + ')',
+    backgroundSize: 'contain',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh'
+  }}>
   </div>);
-  
+
 const Routes = () => (
   <Switch>
-    <Route exact path="/" component={HomePage}/>
-      <Route path="/home" component={HomeDisplay}/>
-    <Route exact path="/login" component={LoginPage}/>
+    <Route exact path="/" component={HomePage} />
+    <Route path="/home" component={HomeDisplay} />
+    <Route exact path="/login" component={LoginPage} />
     <Route exact path="/login2" component={Login2} />
     <Route exact path="/registro" component={Registro} />
-    <Route path="/new" component={CreateProject}/>
-    <Route exact path="/explorar" component={ProjectsPage}/>
-      <Route path="/explorar/:category" component={ProjectsPage}/>
-    <Route path="/userprofile" component={UserProfilePage}/>
-    <Route exact path="/new" component={CreateProject}/>
-    <Route path={`/manage/:projectId`} component={ManagerPage}/>
-    <Route path="/detail/:projectId" component={BlissDetailPage}/>
-    <Route path="/users/:userId" component={PublicProfile}/>
-    <Route path="/admin" component={AdminPanel}/>
-    <Route path="/cart/:rewardId" component={Cart}/>
-    <Route path="/chat" component={ChatPage}/>
+    <Route path="/new" component={CreateProject} />
+    <Route exact path="/explorar" component={ProjectsPage} />
+    <Route path="/explorar/:category" component={ProjectsPage} />
+    <Route path="/userprofile" component={UserProfile} />
+    <Route exact path="/new" component={CreateProject} />
+    <Route path={`/manage/:projectId`} component={ManagerPage} />
+    <Route path="/detail/:projectId" component={BlissDetailPage} />
+    <Route path="/users/:userId" component={PublicProfile} />
+    <Route path="/admin" component={AdminPanel} />
+    <Route path="/cart/:rewardId" component={Cart} />
+    <Route path="/chat" component={ChatPage} />
 
-    <Route component={noMatch}/>
+    <Route component={noMatch} />
   </Switch>
 );
 
