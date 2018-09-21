dcdec10273f7c5bd0333497c6e0c9d28604b5ca0
MefitHp <mefitDev@gmail.com>
2018-09-21 15:16:39 -0500

    Added change banner functionality

--- src/components/userProfile/OwnCampaigns.js
+++ src/components/userProfile/OwnCampaigns.js
@@ -13,14 +13,14 @@ export const OwnCampaigns = ({ fakeFunds, percentage, getDate }) => {
                 return <div key={index} className="campaign-card">
                     <div className="left">
                         <div style={{ margin: '5px' }}>
-                            <img src={campaign.image} alt="Campaign Image" className="img-camp" />
+                            <img src={campaign.image} alt="Campaign" className="img-camp" />
                         </div>
                     </div>
                     <div className="right">
                         <h2>{campaign.name}</h2>
                         <div className="user-box">
                             <div className="img-user">
-                                <img src={campaign.ownerPicture} alt="Owner Image" className="img-ownwer" />
+                                <img src={campaign.ownerPicture} alt="Owner" className="img-ownwer" />
                             </div>
                             <div className="info-user" style={{ margin: 'auto' }}>
                                 <i><i className="fas fa-map-marker-alt" style={{ color: '#3E84FF' }}></i> {campaign.location.city}, {campaign.location.country}. {" "}  <i className="fas fa-tags" style={{ color: '#3E84FF' }}></i> {campaign.category} </i>
@@ -33,9 +33,9 @@ export const OwnCampaigns = ({ fakeFunds, percentage, getDate }) => {
                             <div className="fundTags">
                                 <div style={{ flex: 1, justifyContent: "center" }}>
                                     <div style={{ width: '100%', margin: '0 auto' }}>
-                                        <img src={Flag} alt="Owner Image" className="rocket-icons" />
+                                        <img src={Flag} alt="Owner" className="rocket-icons" />
                                         Meta: <strong>${campaign.fundGoal}</strong> <br />
-                                        <img src={Rocket} alt="Owner Image" className="rocket-icons" />
+                                        <img src={Rocket} alt="Owner " className="rocket-icons" />
                                         Monto recaudado: <strong>${campaign.currentFund}</strong> <br />
                                         <i className="far fa-calendar" style={{ color: '#3E84FF' }}></i> Fecha Limite: <strong>{moment(campaign.date.limit).format("DD MMMM YYYY")}</strong><br />
                                         <i className="far fa-check-square" style={{ color: '#3E84FF' }}></i> Estado: <strong>{campaign.status} <i className="far fa-dot-circle" style={{ color: campaign.status.toLowerCase() === 'publicado' ? 'Chartreuse ' : 'tomato' }}></i></strong>.
--- src/components/userProfile/UserProfile.js
+++ src/components/userProfile/UserProfile.js
@@ -9,20 +9,34 @@ function clickBanner() {
 }
 
 class UserProfile extends Component {
+    state = {
+        user: {
+            bannerPhoto: ""
+        }
+    }
+
+    handleImg = (e) => {
+        let { user } = this.state;
+        let field = e.target.name;
+
+        user[field] = e.target.files[0];
+        this.setState({ user });
+        console.log(this.state.user.bannerPhoto);
+    }
     render() {
         return (
             <div className="wrapper">
                 <header className="profile-banner">
                     <i className="fa fa-camera" aria-hidden="true" title="Cambiar imagen de portada"
                         onClick={clickBanner}>
-                        <input type='file' hidden ref={input => inputBanner = input} accept="image/*" name="file" />
+                        <input type='file' hidden ref={input => inputBanner = input} accept="image/*" name="bannerPhoto" onChange={this.handleImg} />
                     </i>
 
                 </header>
                 <div className="row">
                     <div className="info-section">
                         <div className="profile-container">
-                            <img src="https://scontent.fmex5-1.fna.fbcdn.net/v/t1.0-9/39526443_10204606200150941_266868600390811648_n.jpg?_nc_cat=0&oh=10871406d70bb2de54b6e0a417770140&oe=5BF95C69" alt="" className="profile-picture" onClick={() => alert("Clicked")} />
+                            <img src="https://scontent.fmex5-1.fna.fbcdn.net/v/t1.0-9/39526443_10204606200150941_266868600390811648_n.jpg?_nc_cat=0&oh=10871406d70bb2de54b6e0a417770140&oe=5BF95C69" className="profile-picture" onClick={() => alert("Clicked")} />
                         </div>
                         <br />
                         <section>
